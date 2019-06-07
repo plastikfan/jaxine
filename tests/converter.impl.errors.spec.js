5b698528a080dd2385dcc575ee5e9033157213bd
Plastikfan <plastikfan@zoho.com>
2019-06-07 08:47:21 +0100

    feat(converter): validate coercion.attributes.trim property
    feat #11

---
+++ tests/converter.impl.errors.spec.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function () {
+  const chai = require('chai');
+  chai.use(require('dirty-chai'));
+  const expect = chai.expect;
+  const R = require('ramda');
+
+  const Impl = require('../lib/converter.impl');
+
+  describe('converter.impl:validateOptions [coerce]', () => {
+    const baseSpec = {
+      labels: {
+        element: '_',
+        descendants: '_children',
+        text: '_text'
+      }
+    };
+
+    context('Invalid coercion options', () => {
+      const tests = [
+        {
+          given: 'coercion with typo in "trim" property',
+          spec: () => {
+            return R.set(R.lensProp('coercion'), {
+              attributes: {
+                trim: 'terue'
+              }
+            })(baseSpec);
+          }
+        }
+      ];
+
+      R.forEach((t) => {
+        it(`should: throw, given: ${t.given}`, () => {
+          expect(() => {
+            Impl.validateSpec(t.spec());
+          }).to.throw();
+        });
+      })(tests);
+    });
+  });
+})();
---
+++ tests/converter.impl.success.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+(function () {
+  const chai = require('chai');
+  chai.use(require('dirty-chai'));
+  const assert = chai.assert;
+
+  const R = require('ramda');
+  const Impl = require('../lib/converter.impl');
+
+  describe('converter.impl:validateOptions [coerce]', () => {
+    const baseSpec = {
+      labels: {
+        element: '_',
+        descendants: '_children',
+        text: '_text'
+      }
+    };
+
+    context('Valid coercion options', () => {
+      const tests = [
+        {
+          given: 'coercion with only trim property',
+          spec: () => {
+            return R.set(R.lensProp('coercion'), {
+              attributes: {
+                trim: true
+              }
+            })(baseSpec);
+          }
+        },
+        {
+          given: 'spec without coercion property',
+          spec: () => baseSpec
+        }
+      ];
+
+      R.forEach((t) => {
+        it(`should: not fail validation, given: ${t.given}`, () => {
+          try {
+            Impl.validateSpec(t.spec());
+          } catch (err) {
+            assert.fail(err.message);
+          }
+        });
+      })(tests);
+    });
+  });
+})();
